0edda8ab361b586ec324539eb0cef70c2743a65d
Janani NA <janani2285@gmail.com>
2020-06-14 22:34:02 -0700

    Display and hide function added

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,14 @@
 var timer =0;
 
 document.getElementById("timer").textContent = "Time: "+timer;
+
+var strPageEl = document.querySelector("#strPage");
+var questionsEl = document.querySelector("#questions");
+
+var strQuizBtn = document.getElementById("startQuiz");
+strQuizBtn.addEventListener("click",startQuiz);
+
+function startQuiz(event){
+    strPageEl.classList.add("hide");
+    questionsEl.classList.remove("hide");
+}
