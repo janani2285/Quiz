b5af253b563471eb69e257bbe822f7c2101a9753
Janani NA <janani2285@gmail.com>
2020-06-15 23:15:40 -0700

    SetInterval added

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,46 +1,104 @@
-var timer =0;
+var timer = 0;
 
-document.getElementById("timer").textContent = "Time: "+timer;
+document.getElementById("timer").textContent = "Time: " + timer;
 
 var strPageEl = document.querySelector("#strPage");
 var quizEl = document.querySelector("#quizContent");
 var questionEl = document.querySelector("#question");
 var answerElList = document.querySelector("#ansChoice");
 
+var secondsLeft = 75;
+
 var questAnswerArr = [
     {
-      question: "Which is not a JavaScript data type?",
-      answers: ["Number","Strings","Arrays","Boolean"],
-      correctAnswer: "Arrays"
-    }];
+        question: "Which is not a JavaScript data type?",
+        answers: ["number", "strings", "alerts", "boolean"],
+        correctAnswer: "alerts"
+    }
+    /*,
+    {
+        question: "The condition in an if/else statement is enclosed within?",
+        answers: ["quotes","curly brackets","parentheses","square brackets"],
+        correctAnswer: "parentheses"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store?",
+        answers: ["numbers","other arrays","objects","all the above"],
+        correctAnswer: "all the above"
+    },
+    {
+        question: "When being assignmed to variables, String values must be enclosed within?",
+        answers: ["commas","curly brackets","quotes","parentheses"],
+        correctAnswer: "quotes"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is?",
+        answers: ["JavaScript","terminal/bash","for loop","console.log"],
+        correctAnswer: "console.log"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is?",
+        answers: ["JavaScript","terminal/bash","for loop","console.log"],
+        correctAnswer: "console.log"
+    }*/
+];
 
 
 
 var strQuizBtn = document.getElementById("startQuiz");
-strQuizBtn.addEventListener("click",startQuiz);
+strQuizBtn.addEventListener("click", startQuiz);
 
-function startQuiz(event){
+function startQuiz(event) {
     strPageEl.classList.add("hide");
     quizEl.classList.remove("hide");
-    answerElList.innerHTML = "";
 
-    for (var i = 0; i < questAnswerArr.length; i++) {
-        var questionItem = questAnswerArr[i];
-        questionEl.textContent = questionItem.question;
-        var answerItem = questAnswerArr[i].answers;
-        var correctAns = questAnswerArr[i].correctAnswer;
 
-        for(var j=0;j<answerItem.length;j++){
-            var li = document.createElement("li");
-            var btn = document.createElement("button");
-            btn.textContent = answerItem[j];
-            li.setAttribute("id",j);
+    var timerInterval = setInterval(function () {
 
-            btn.setAttribute("data-index",correctAns);
-            btn.setAttribute("class","btn btn-primary mb-2");
-            li.appendChild(btn);
-            answerElList.appendChild(li);
+        timer = secondsLeft--;
+
+
+        document.getElementById("timer").textContent = "Time: " + timer;
+
+        for (var i = 0; i < questAnswerArr.length; i++) {
+            answerElList.innerHTML = "";
+
+            var questionItem = questAnswerArr[i];
+            questionEl.textContent = questionItem.question;
+            var answerItem = questAnswerArr[i].answers;
+            var correctAns = questAnswerArr[i].correctAnswer;
+
+            for (var j = 0; j < answerItem.length; j++) {
+
+                var li = document.createElement("li");
+                var btn = document.createElement("button");
+                btn.textContent = answerItem[j];
+                li.setAttribute("id", j);
+                if (answerItem[j] === correctAns) {
+                    btn.setAttribute("data-index", correctAns);
+                } else {
+                    btn.setAttribute("data-index", "incorrect");
+                }
+
+                btn.setAttribute("class", "button btn btn-primary mb-2");
+                li.appendChild(btn);
+                answerElList.appendChild(li);
+
+                btn.addEventListener("click", function (event) {
+                    console.log("event: " + event);
+                    console.log("event.target: " + event.target);
+                    var targetBtn = event.target;
+                    console.log("Answer selected: " + targetBtn.textContent);
+                });
+            }
+
         }
-        
-      }
+        if (secondsLeft === 0) {
+
+            clearInterval(timerInterval);
+            //TODO:declare the function
+            //  showEnterHighScore();
+        }
+
+    }, 1000);
 }
