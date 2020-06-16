c21e0e1156f6b4202f8f33730ee4ae46b67208b9
Janani NA <janani2285@gmail.com>
2020-06-16 16:32:52 -0700

    Displaying multiple questions

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -14,14 +14,13 @@ var questAnswerArr = [
         question: "Which is not a JavaScript data type?",
         answers: ["number", "strings", "alerts", "boolean"],
         correctAnswer: "alerts"
-    }
-    /*,
-    {
-        question: "The condition in an if/else statement is enclosed within?",
-        answers: ["quotes","curly brackets","parentheses","square brackets"],
-        correctAnswer: "parentheses"
     },
     {
+        question: "The condition in an if/else statement is enclosed within?",
+        answers: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        correctAnswer: "parentheses"
+    }
+    /*{
         question: "Arrays in JavaScript can be used to store?",
         answers: ["numbers","other arrays","objects","all the above"],
         correctAnswer: "all the above"
@@ -49,62 +48,71 @@ var strQuizBtn = document.getElementById("startQuiz");
 var ackBlock = document.getElementById("acknowlegeBlock");
 var ackDisplay = document.getElementById("ack");
 strQuizBtn.addEventListener("click", startQuiz);
+var index = 0;
 
 function startQuiz(event) {
-    strPageEl.classList.add("hide");
-    quizEl.classList.remove("hide");
-
 
     var timerInterval = setInterval(function () {
+        strPageEl.classList.add("hide");
+        quizEl.classList.remove("hide");
 
         timer = secondsLeft--;
 
-
         document.getElementById("timer").textContent = "Time: " + timer;
 
-        for (var i = 0; i < questAnswerArr.length; i++) {
-            answerElList.innerHTML = "";
+        renderQuestions();
 
-            var questionItem = questAnswerArr[i];
-            questionEl.textContent = questionItem.question;
-            var answerItem = questAnswerArr[i].answers;
-            var correctAns = questAnswerArr[i].correctAnswer;
 
-            for (var j = 0; j < answerItem.length; j++) {
-
-                var li = document.createElement("li");
-                var btn = document.createElement("button");
-                btn.textContent = answerItem[j];
-                li.setAttribute("id", j);
-                if (answerItem[j] === correctAns) {
-                    btn.setAttribute("data-index", "Correct");
-                } else {
-                    btn.setAttribute("data-index", "Incorrect");
-                }
-
-                btn.setAttribute("class", "button btn btn-primary mb-2");
-                li.appendChild(btn);
-                answerElList.appendChild(li);
-
-                btn.addEventListener("click", function (event) {
-                    console.log("event: " + event);
-                    console.log("event.target: " + event.target);
-                    var targetBtn = event.target;
-                    console.log("Answer selected: " + targetBtn.textContent);
-                    
-                        ackBlock.classList.remove("hide");
-                        ackDisplay.textContent = targetBtn.getAttribute("data-index");
-                    
-                });
-            }
-
-        }
         if (secondsLeft === 0) {
 
             clearInterval(timerInterval);
+            alert("End of quiz");
             //TODO:declare the function
             //  showEnterHighScore();
         }
 
     }, 1000);
+}
+
+function renderQuestions() {
+
+    console.log("inside render")
+    if (index < questAnswerArr.length) {
+
+        answerElList.innerHTML = "";
+
+        var questionItem = questAnswerArr[index];
+        questionEl.textContent = questionItem.question;
+        var answerItem = questAnswerArr[index].answers;
+        var correctAns = questAnswerArr[index].correctAnswer;
+
+        for (var j = 0; j < answerItem.length; j++) {
+
+            var li = document.createElement("li");
+            var btn = document.createElement("button");
+            btn.textContent = answerItem[j];
+            li.setAttribute("id", j);
+            if (answerItem[j] === correctAns) {
+                btn.setAttribute("data-index", "Correct");
+            } else {
+                btn.setAttribute("data-index", "Incorrect");
+            }
+
+            btn.setAttribute("class", "button btn btn-primary mb-2");
+            li.appendChild(btn);
+            answerElList.appendChild(li);
+         //   ackBlock.classList.add("hide");
+
+            btn.addEventListener("click", function (event) {
+                var targetBtn = event.target;
+                ackBlock.classList.remove("hide");
+                ackDisplay.textContent = targetBtn.getAttribute("data-index");
+                console.log("inside eventlistener-before calling start quiz again");
+                index++;
+                renderQuestions();
+
+            });
+        }
+
+    }
 }
