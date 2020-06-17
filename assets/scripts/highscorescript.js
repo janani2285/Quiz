56ec56db3760ef5801d9c270382578dff88e663e
Janani NA <janani2285@gmail.com>
2020-06-16 23:44:51 -0700

    displaying highscorelist

--- assets/scripts/highscorescript.js
+++ assets/scripts/highscorescript.js
@@ -6,12 +6,12 @@ function init(){
     var scoreBlockEl = document.getElementById("scoresBlock");
     var scoresListEl = document.getElementById("scoresList");
 
-   /* for(var i=0;i<scoreEl.length;i++) {
-      //  scoreEl[i].classList.add(i % 2 === 0 ? "even" : "odd");
+    for(var i=0;i<scoreBlockEl.length;i++) {
+      //  scoreBlockEl[i].classList.add(i % 2 === 0 ? "even" : "odd");
       //or
-      scoreEl[i].style["background-color"] = i % 2 === 0 ? "grey" : "white";
-      scoreEl[i].style["color"] = i % 2 === 0 ? "white" : "black";
-    }*/
+      scoreBlockEl[i].style["background-color"] = i % 2 === 0 ? "grey" : "white";
+      scoreBlockEl[i].style["color"] = i % 2 === 0 ? "white" : "black";
+    }
     
     var scoreLocalStorageObjArr = JSON.parse(localStorage.getItem("score"));
     if(scoreLocalStorageObjArr === null){
@@ -23,8 +23,20 @@ function init(){
         var li = document.createElement("li");
         li.textContent = scoreLocalStorageObjArr[j].user + " : " + scoreLocalStorageObjArr[j].score;
         li.style.fontSize = "20px";
+        li.style.marginBottom="20px";
+        li.style.marginTop="20px";
+     /*   if(j%2 === 0){
+          scoresListEl.style.backgroundColor = "grey";
+          scoresListEl.style.color = "white";
+        }else{
+          scoresListEl.style.backgroundColor = "black";
+          scoresListEl.style.color = "white";
+        }*/
+      
         scoresListEl.appendChild(li);
+       
       }
+  
     }
     
 }
